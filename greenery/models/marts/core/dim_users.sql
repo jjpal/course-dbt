27f278aea76a7ec9db686e59932fe4ca166a3b34
jjpal <jjpal-projects@protonmail.com>
2022-06-26 04:14:03 +0000

    update

--- greenery/models/marts/core/dim_users.sql
+++ greenery/models/marts/core/dim_users.sql
@@ -5,16 +5,20 @@
 }}
 
 select
-       u.user_guid
-       , u.first_name
-       , u.last_name
+       intu.user_guid
+       , intu.user_full_name
        , u.email as user_email
        , u.phone_number as user_phone_number
+       , intu.user_registered_on
        , age(u.updated_at_utc, u.users_created_at_utc) as last_update_activity
+       , intu.user_account_age
        , a.address as user_address
        , a.zipcode as user_zip_code
        , a.state as user_state
        , a.country as user_country
-from {{ ref('stg_greenery__users') }} u
+from {{ ref('int_users_registered') }} intu
+left join {{ ref('stg_greenery__users') }} u
+on intu.user_guid = u.user_guid
 left join {{ ref('stg_greenery__addresses') }} a
 on u.address_guid = a.address_guid
+
